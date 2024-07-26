cea7e9e8ca3a7492a252a096745b28d3bcbaf611
andrewy9 <andrew.yang993@gmail.com>
2024-07-26 22:16:59 +1200

    initial styles set up

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,29 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import NavBar from "@/components/layouts/NavBar/NavBar";
+import "@/styles/global.css";
+import StyledComponentsRegistry from "@/lib/registry";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-    title: "mirae hagwon",
-    description: "Place to learn and grow",
+  title: "mirae hagwon",
+  description: "Place to learn and grow",
 };
 
 export default function RootLayout({
-    children,
+  children,
 }: Readonly<{
-    children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-    return (
-        <html lang="en">
-            <body className={inter.className}>{children}</body>
-        </html>
-    );
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <StyledComponentsRegistry>
+          <NavBar />
+          {children}
+        </StyledComponentsRegistry>
+      </body>
+    </html>
+  );
 }
